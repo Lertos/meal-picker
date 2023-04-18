606c4644e0ff76416eaab135ab55a527068afe6d
Lertos <deetasticgaming@gmail.com>
2023-04-18 19:49:20 -0300

    Initial commit

---
+++ app/src/main/java/com/lertos/mealpicker/PageHome.java
@@ -0,0 +1,16 @@
+package com.lertos.mealpicker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PageHome extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_page_home);
+    }
+}
