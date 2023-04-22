bad345f85bf078113b832b22d47ab9b22b1788ec
Lertos <deetasticgaming@gmail.com>
2023-04-22 10:38:30 -0300

    Adding the logic to switch between fragments

--- app/src/main/java/com/lertos/mealpicker/PageHome.java
+++ app/src/main/java/com/lertos/mealpicker/PageHome.java
@@ -3,14 +3,58 @@ package com.lertos.mealpicker;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.lertos.mealpicker.databinding.LayoutPageHomeBinding;
 
 public class PageHome extends AppCompatActivity {
 
+    private LayoutPageHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.layout_page_home);
+        binding = LayoutPageHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Set the initial screen
+        replaceFragment(new FragmentSearchMeal());
+
+        setupBindings();
     }
+
+    private void setupBindings() {
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navBtnSearch:
+                    replaceFragment(new FragmentSearchMeal());
+                    break;
+                case R.id.navBtnAdd:
+                    replaceFragment(new FragmentAddMeal());
+                    break;
+                case R.id.navBtnPlanner:
+                    replaceFragment(new FragmentMealPlanner());
+                    break;
+                case R.id.navBtnTags:
+                    replaceFragment(new FragmentTagManager());
+                    break;
+                case R.id.navBtnSettings:
+                    replaceFragment(new FragmentSettings());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
