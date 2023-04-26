badc61b2bead190327b35068e7b16b4b1f4ab632
Lertos <deetasticgaming@gmail.com>
2023-04-26 19:07:34 -0300

    Adding initial variables, methods, and constructor

--- app/src/main/java/com/lertos/mealpicker/model/MealManager.java
+++ app/src/main/java/com/lertos/mealpicker/model/MealManager.java
@@ -1,4 +1,29 @@
 package com.lertos.mealpicker.model;
 
+import java.util.ArrayList;
+
 public class MealManager {
+
+    private ArrayList<Meal> mealList;
+
+    public MealManager(ArrayList<Meal> mealList) {
+        this.mealList = new ArrayList<>();
+
+        loadSavedMeals();
+    }
+
+    private void loadSavedMeals() {
+        //TODO: Load the meals from the local save file
+    }
+
+    public boolean addMeal(Meal newMeal) {
+        for (Meal meal : mealList) {
+            if (meal.getTitle().equalsIgnoreCase(newMeal.getTitle()))
+                return false;
+        }
+        mealList.add(newMeal);
+
+        return true;
+    }
+
 }
