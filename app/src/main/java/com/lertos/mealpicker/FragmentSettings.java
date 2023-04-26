45323e612faace77e71c4f6e6f14acc514709b07
Lertos <deetasticgaming@gmail.com>
2023-04-25 21:29:34 -0300

    Adding logic to actually change the theme via the toggle button

--- app/src/main/java/com/lertos/mealpicker/FragmentSettings.java
+++ app/src/main/java/com/lertos/mealpicker/FragmentSettings.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ToggleButton;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import com.lertos.mealpicker.model.DataManager;
+
 public class FragmentSettings extends Fragment {
+
+    private ToggleButton tbDarkMode;
 
     public FragmentSettings() {
     }
@@ -20,6 +26,23 @@ public class FragmentSettings extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        tbDarkMode = view.findViewById(R.id.tbDarkMode);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            tbDarkMode.setChecked(true);
+        else
+            tbDarkMode.setChecked(false);
+
+        tbDarkMode.setOnClickListener(btnView -> {
+            if (tbDarkMode.isChecked())
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+            //This is to tell the main activity to reload the settings page since the activity restarts after switching themes; yuck!
+            DataManager.getInstance().setChangedDayNightTheme(true);
+        });
 
         return view;
     }
