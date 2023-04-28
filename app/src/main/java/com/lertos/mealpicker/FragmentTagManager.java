bf1ade213173fe8f59dd5d876a1c7376d4f32cda
Lertos <deetasticgaming@gmail.com>
2023-04-28 20:58:58 -0300

    Creating the recycler view and assigning the adapter

--- app/src/main/java/com/lertos/mealpicker/FragmentTagManager.java
+++ app/src/main/java/com/lertos/mealpicker/FragmentTagManager.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lertos.mealpicker.adapters.AdapterTagList;
+import com.lertos.mealpicker.model.DataManager;
 
 public class FragmentTagManager extends Fragment {
 
@@ -20,6 +25,14 @@ public class FragmentTagManager extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tag_manager, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.rvTagsTimeToMake);
+
+        AdapterTagList adapterTagList = new AdapterTagList();
+        adapterTagList.setDataList(DataManager.getInstance().getTags().getTagsTimeToMake());
+
+        recyclerView.setAdapter(adapterTagList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
 
         return view;
     }
