2cd73910fdb19114fa301c24bc502b9f8456aa37
Lertos <deetasticgaming@gmail.com>
2023-04-30 18:59:56 -0300

    Adding the logic for the meal list recyclerview

--- app/src/main/java/com/lertos/mealpicker/FragmentSearchMeal.java
+++ app/src/main/java/com/lertos/mealpicker/FragmentSearchMeal.java
@@ -6,8 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lertos.mealpicker.adapters.AdapterMealList;
+import com.lertos.mealpicker.model.DataManager;
+import com.lertos.mealpicker.model.Meal;
+
+import java.util.List;
 
 public class FragmentSearchMeal extends Fragment {
+
+    private View view;
+    private RecyclerView recyclerView;
+    private AdapterMealList adapterMealList;
 
     public FragmentSearchMeal() {
     }
@@ -19,8 +31,21 @@ public class FragmentSearchMeal extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search_meal, container, false);
+        view = inflater.inflate(R.layout.fragment_search_meal, container, false);
+
+        recyclerView = view.findViewById(R.id.rvMealList);
+
+        //TODO: Get a filtered list from the MealManager
+        setAdapterMealList(DataManager.getInstance().getMeals().getMeals());
 
         return view;
+    }
+
+    private void setAdapterMealList(List<Meal> mealList) {
+        adapterMealList = new AdapterMealList();
+        adapterMealList.setDataList(mealList);
+
+        recyclerView.setAdapter(adapterMealList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
     }
 }
