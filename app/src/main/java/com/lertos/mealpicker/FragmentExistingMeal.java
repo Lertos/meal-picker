935be28aead2e34b262c01906825e68892507dc3
Lertos <deetasticgaming@gmail.com>
2023-05-05 19:08:11 -0300

    Adding new existing meal page

---
+++ app/src/main/java/com/lertos/mealpicker/FragmentExistingMeal.java
@@ -0,0 +1,27 @@
+package com.lertos.mealpicker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentExistingMeal extends Fragment {
+
+    public FragmentExistingMeal() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_existing_meal, container, false);
+
+        return view;
+    }
+
+}
