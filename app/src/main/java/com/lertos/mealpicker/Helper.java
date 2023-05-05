cbedabff386c528976e74d7852cb7b4be75c668b
Lertos <deetasticgaming@gmail.com>
2023-05-05 17:33:00 -0300

    Fixing crash bug

--- app/src/main/java/com/lertos/mealpicker/Helper.java
+++ app/src/main/java/com/lertos/mealpicker/Helper.java
@@ -8,8 +8,11 @@ import androidx.fragment.app.FragmentActivity;
 public class Helper {
 
     public static void hideKeyboard(FragmentActivity activity) {
-        InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        //Checks if there is an active keyboard or not
+        if (activity.getCurrentFocus() != null) {
+            InputMethodManager inputManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
     }
 
 }
