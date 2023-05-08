295595a818f9948f48d37c456369f87d3b2e993c
Lertos <deetasticgaming@gmail.com>
2023-05-08 18:45:09 -0300

    Adding defaults, getters, and setters

--- app/src/main/java/com/lertos/mealpicker/model/Settings.java
+++ app/src/main/java/com/lertos/mealpicker/model/Settings.java
@@ -2,10 +2,10 @@ package com.lertos.mealpicker.model;
 
 public class Settings {
 
-    private boolean useMinutesOnly;
-    private boolean useDarkMode;
-    private boolean closeKeyboardAfterTagCreation;
-    private boolean resetFieldsAfterMealCreation;
+    private boolean useMinutesOnly = false;
+    private boolean useDarkMode = false;
+    private boolean closeKeyboardAfterTagCreation = false;
+    private boolean resetFieldsAfterMealCreation = false;
 
     public Settings() {
         loadSettings();
@@ -15,4 +15,35 @@ public class Settings {
         //TODO: Load settings from the local config file/create new file if it doesn't exist with defaults
     }
 
+    public boolean useMinutesOnly() {
+        return useMinutesOnly;
+    }
+
+    public void setUseMinutesOnly(boolean useMinutesOnly) {
+        this.useMinutesOnly = useMinutesOnly;
+    }
+
+    public boolean useDarkMode() {
+        return useDarkMode;
+    }
+
+    public void setUseDarkMode(boolean useDarkMode) {
+        this.useDarkMode = useDarkMode;
+    }
+
+    public boolean useCloseKeyboardAfterTagCreation() {
+        return closeKeyboardAfterTagCreation;
+    }
+
+    public void setCloseKeyboardAfterTagCreation(boolean closeKeyboardAfterTagCreation) {
+        this.closeKeyboardAfterTagCreation = closeKeyboardAfterTagCreation;
+    }
+
+    public boolean useResetFieldsAfterMealCreation() {
+        return resetFieldsAfterMealCreation;
+    }
+
+    public void setResetFieldsAfterMealCreation(boolean resetFieldsAfterMealCreation) {
+        this.resetFieldsAfterMealCreation = resetFieldsAfterMealCreation;
+    }
 }
