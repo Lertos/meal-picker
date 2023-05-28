dc26c4e8f4ba3e599da256a8636336b5b3e9c4a6
Lertos <deetasticgaming@gmail.com>
2023-05-28 20:42:05 -0300

    Don't need context as a variable

--- app/src/main/java/com/lertos/mealpicker/model/FileManager.java
+++ app/src/main/java/com/lertos/mealpicker/model/FileManager.java
@@ -3,14 +3,11 @@ package com.lertos.mealpicker.model;
 import android.content.Context;
 
 public class FileManager {
-    private final Context context;
     private DataFile settingsFile;
     private DataFile tagFile;
     private DataFile mealFile;
 
     public FileManager(Context context) {
-        this.context = context;
-
         this.settingsFile = new DataFile(context, "settings.txt");
         this.tagFile = new DataFile(context, "tags.txt");
         this.mealFile = new DataFile(context, "meals.txt");
